11d30fabf7cffdc8519c994a4773e2d954826ad1
Aman Kumar Patel <107243321+amanpatel3045@users.noreply.github.com>
2022-11-27 22:02:50 +0530

    Create code.js

---
+++ Catch that 420!/code.js
@@ -0,0 +1,10 @@
+function catch420(n) {
+  //write code here
+var a=  n.toString();
+
+    if(a.includes("420")){
+        console.log("Caught");
+    }else{
+        console.log("Escaped");
+    }
+}
