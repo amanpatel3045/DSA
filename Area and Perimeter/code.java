45cf35a3f9624dbb9031ebe4edd34cbdc7b8bd3e
Aman Kumar Patel <107243321+amanpatel3045@users.noreply.github.com>
2022-12-11 23:56:09 +0530

    Create code.java

---
+++ Area and Perimeter/code.java
@@ -0,0 +1,7 @@
+public static void areaAndPerimeter(int l1,int b1,int l2,int b2){
+    //write your code here
+    boolean aman=l1*b1>l2*b2;
+    boolean atul=2*(l1+b1)>2*(l2+b2);
+    System.out.println(aman);
+    System.out.println(atul);
+  }
