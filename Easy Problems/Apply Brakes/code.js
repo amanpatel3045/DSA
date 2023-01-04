f331b9c5a04b46c5f684d282fa39501f0fa3e32d
Aman Kumar Patel <107243321+amanpatel3045@users.noreply.github.com>
2023-01-04 23:26:47 +0530

    Create code.js

---
+++ Easy Problems/Apply Brakes/code.js
@@ -0,0 +1,13 @@
+function applyBrakes(distance, time) {
+    //Write code here
+    var speed=distance/time;
+    if(speed>40)
+    {
+        console.log("Apply Brake");
+    }
+    else
+    {
+        console.log("Keep Going");
+    }
+    
+}
