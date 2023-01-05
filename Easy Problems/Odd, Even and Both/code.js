9c70016d5bcdd34e32eef073dc46b7489fe47280
Aman Kumar Patel <107243321+amanpatel3045@users.noreply.github.com>
2023-01-05 23:24:37 +0530

    Create code.js

---
+++ Easy Problems/Odd, Even and Both/code.js
@@ -0,0 +1,15 @@
+function oddEvenBoth(one, two) {
+    // Write code here
+    if(one%2===0 && two%2===0)
+    {
+        console.log("Even");
+    }
+    else if(one%2===1 && two%2==1)
+    {
+        console.log("Odd");
+    }
+    else
+    {
+        console.log("Even-Odd");
+    }
+}
