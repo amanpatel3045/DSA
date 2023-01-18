92c9ff55f68811c91378f51cbede9502f7844bd9
Aman Kumar Patel <107243321+amanpatel3045@users.noreply.github.com>
2023-01-18 22:08:57 +0530

    Create code.java

---
+++ Easy Problems/The Ashes! (But One day)/code.java
@@ -0,0 +1,10 @@
+public static void theAshes(int Australia,int England){
+    //write your code here
+    if(Australia>England){
+        System.out.println("Australia");
+    }else if(Australia<England){
+        System.out.println("England");
+    }else if(Australia==England){
+        System.out.println("Tie");
+    }
+  }
