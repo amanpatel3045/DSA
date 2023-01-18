6d5ce2fb49b4c4a46453870263caf658fc5fc427
Aman Kumar Patel <107243321+amanpatel3045@users.noreply.github.com>
2023-01-18 22:11:38 +0530

    Create code.java

---
+++ Easy Problems/Move 2 Ahead II/code.java
@@ -0,0 +1,14 @@
+public static void moveTwoAheadII(int num){
+    //write your code here
+    for(int i=1;i<=num;i++)
+    {
+        if(i%2==0)
+        {
+            System.out.println(i);
+        }
+        else
+        {
+         continue;
+        }
+    }
+  }
