9f921a10c086d9cb14932054e18296cd88ec950f
Aman Kumar Patel <107243321+amanpatel3045@users.noreply.github.com>
2023-01-19 22:16:42 +0530

    Create code.java

---
+++ Even Array/code.java
@@ -0,0 +1,11 @@
+public static void evenArray(int n, int arr[]){
+    //write your code here
+    for(int i=0;i<=n-1;i++)
+    {
+        if(arr[i]%2==0)
+        {
+            System.out.println(arr[i]);
+        }
+       
+    }
+  }
