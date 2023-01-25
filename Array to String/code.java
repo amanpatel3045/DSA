397d17018e181ed5a7f71b494b1ad150d5c6dee4
Aman Kumar Patel <107243321+amanpatel3045@users.noreply.github.com>
2023-01-25 21:19:24 +0530

    Create code.java

---
+++ Array to String/code.java
@@ -0,0 +1,10 @@
+public static void arrayToString(int n,int[] arr){
+    //write your code here
+    for(int i=0;i<n;i++){
+        if(arr[i]>0){
+            System.out.print(arr[i]);
+        }else{
+            System.out.print(0);
+        }
+    }
+  }
