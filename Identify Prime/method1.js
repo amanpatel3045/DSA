c5cda14b79ba183fd1b5362c26aac1e7b27b35e6
Aman Kumar Patel <107243321+amanpatel3045@users.noreply.github.com>
2023-03-31 23:33:46 +0530

    m1

---
+++ Identify Prime/method1.js
@@ -0,0 +1,12 @@
+var count = 0;
+for (var i = 1; i <= num; i++) {
+    if (num % i == 0) {
+        count++;
+    }
+}
+if (count == 2) {
+    console.log("Yes");
+}
+else {
+    console.log("No");
+}
